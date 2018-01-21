7cd4a398ba8e844350dc4a6e6f947c7793cf26dd
junwen yang <wsklxsx@126.com>
2018-01-21 21:27:59 +0000

    different color

--- lib/ipd.rb
+++ lib/ipd.rb
@@ -5,16 +5,27 @@ module Ipd
   # Your code goes here...
   def self.addElement
       puts "add Element"
-      files = "#{`tail log/development.log`}".scan(/\s[a-z]+\/\S+[erb|haml|slim]\s/)
-      color = ['lightblue','yellow','pink','blue','green']
+      lines = "#{`tail log/development.log`}".split("\n")
+      files = []
+      times = []
+      for l in lines
+        # match the template file and the time cost on it
+        f = l.scan(/\s[a-z]+\/\S+[erb|haml|slim]\s/)
+        t = l.match(/[0-9\.]+ms/)
+        if f.length > 0 and t.length > 0
+          files.push(f[0].gsub(/\s+/, "").split('.')[0])
+          times.push(t[0])
+        end 
+      end
+      color = ['lightblue','yellow','pink','red','green','grey']
       index = 0
       for f in files
-        f = f.gsub(/\s+/, "").split('.')[0]
         puts f
+        # if the template doesn't start from an div, then this surrounding doens't work
         Deface::Override.new(:virtual_path => f, 
                        :name => "example-1", 
                        :surround => "div", 
-                       :text => "<div style='background-color: #{color[index]};'><%= render_original %></div>")
+                       :text => "<div style='background-color: #{color[index]};'><p>#{times[index]}</p><%= render_original %></div>")
         
         index = index + 1 
       end
