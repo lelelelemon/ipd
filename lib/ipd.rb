48025a1ec0f433a36635fcfb475afb0c8d916b6b
junwen yang <wsklxsx@126.com>
2018-01-21 04:41:15 +0000

    change color for each different template

--- lib/ipd.rb
+++ lib/ipd.rb
@@ -3,7 +3,26 @@ require 'ipd/railtie' if defined?(Rails)
 
 module Ipd
   # Your code goes here...
-  def self.addElement()
-     files = "#{`tail log/development.log`}".scan(/\s[a-z]+\/\S+[erb|haml|slim]\s/)
+  def self.addElement
+      puts "add Element"
+      files = "#{`tail log/development.log`}".scan(/\s[a-z]+\/\S+[erb|haml|slim]\s/)
+      color = ['lightblue','yellow','pink','blue','green']
+      index = 0
+      for f in files
+        f = f.gsub(/\s+/, "").split('.')[0]
+        puts f
+        Deface::Override.new(:virtual_path => f, 
+                       :name => "example-1", 
+                       :surround => "div", 
+                       :text => "<div style='background-color: #{color[index]};'><%= render_original %></div>")
+        
+        index = index + 1 
+      end
   end
+end
+
+if defined?(Rails::Railtie)
+  require 'ipd/railtie'
+elsif defined?(Rails::Initializer)
+  raise "ipd 3.0 is not compatible with Rails 2.3 or older"
 end
--- lib/ipd/railtie.rb
+++ lib/ipd/railtie.rb
@@ -2,11 +2,8 @@ module Ipd
     class Railtie < Rails::Railtie
         initializer 'ipd' do |app|
             ActiveSupport.on_load :action_controller do 
-                puts "on load"
             end
             ActiveSupport.on_load :action_view do
-                files = "#{`tail log/development.log`}".scan(/\s[a-z]+\/\S+[erb|haml|slim]\s/)
-                puts files
             end
         end
     end
