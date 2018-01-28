74891c5aec8580f54fb3b327142732eb7653b159
j w <wsklxsx@126.com>
2018-01-28 21:59:11 +0000

    test

--- lib/ipd.rb
+++ lib/ipd.rb
@@ -7,28 +7,11 @@ module Ipd
   @times = []
   def self.addElement
       color = ['lightblue','yellow','pink','lightgreen','grey']
-      puts @files.length
-      if(@files.length > 0)
-        for i in 0..(@files.length-1)
-            f = @files[i]
-            t = @times[i]
-            #Deface::Override.new(:virtual_path => f, 
-            #   :name => "example-1", 
-            #   :surround => "div", 
-            #   :text => "<div id='div-#{i}' style='background-color: #{color[i]};'><p align='center'><strong>#{f}</strong>: #{t}ms <button id='button-#{i}' onclick='delete()'>Delete</button></p><%= render_original %></div><script>document.getElementById('button-#{i}').addEventListener('click', function(){document.getElementById('div-#{i}').style.display = 'none';;});</script>")
-        end
-              
-        @files = []
-        @times = []
-        return
-      end 
-      ActiveSupport::Notifications.subscribe('render_template.action_view') do |name, started, finished, unique_id, payload|
-        Rails.logger.info "#{finished - started} Rendered #{payload[:identifier]} #{@files.length} !"
-        for i in 0..(@files.length-1)
-          f = @files[i]
-          t = @times[i]
-        end 
-        rel_path = payload[:identifier].split("views/")
+      render_events = []
+      query_events = []
+"""
+      ActiveSupport::Notifications.subscribe('render_template.action_view') do |name, started, finished, unique_id, payload, children|
+        rel_path = payload[:identifier].split('views/')
         if rel_path.length >= 2
           f = rel_path[1].scan(/[a-z]+\/\S+[erb|haml|slim]/)[0].split('.')[0]
           if(!@files.include?f)
@@ -41,15 +24,42 @@ module Ipd
       end
       ActiveSupport::Notifications.subscribe('render_partial.action_view') do |name, started, finished, unique_id, payload|
         # your own custom stuff
-        #Rails.logger.info "#{finished - started} Rendered #{payload[:identifier]}!"
-        f = payload[:identifier].split("views/")[1].scan(/[a-z]+\/\S+[erb|haml|slim]/)[0].split('.')[0]
-        if(!@files.include?f)
-          @files << f 
-          @times << (finished - started) * 1000
+        
+        rel_path = payload[:identifier].split('views/')
+        if rel_path.length >= 2
+          f = rel_path[1].scan(/[a-z]+\/\S+[erb|haml|slim]/)[0].split('.')[0]
+          if(!@files.include?f)
+            @files << f 
+            t = (finished - started) * 1000
+            @times << t
+              
+          end
         end
       end
-      
-
+"""
+      ActiveSupport::Notifications.subscribe('render_template.action_view') do |*args|
+        #Rails.logger.info "#{finished - started} Rendered #{payload[:identifier]} #{@files.length} Children #{children}"
+      	event = ActiveSupport::Notifications::Event.new(*args)
+      	render_events << event
+      end
+      ActiveSupport::Notifications.subscribe('render_partial.action_view') do |*args|
+      	event = ActiveSupport::Notifications::Event.new(*args)
+      	render_events << event
+      end
+      ActiveSupport::Notifications.subscribe('sql.active_record') do |*args|
+      	event = ActiveSupport::Notifications::Event.new(*args)
+      	query_events << event
+      end
+      render_events.each do |re|
+        puts re.name
+        query_events.each do|qe|
+          puts qe.name
+          if(re.parent_of?(qe)) 
+            Rails.logger.info "Parent #{re.name} -> #{qe.name}"
+          end
+        end
+      end
+    
       ''
   end
 end
@@ -60,12 +70,4 @@ elsif defined?(Rails::Initializer)
   raise "ipd 3.0 is not compatible with Rails 2.3 or older"
 end
 
-backtrace = Rails.backtrace_cleaner.clean caller
 
-relevant_caller_line = backtrace.detect do |caller_line|
-  !caller_line.include?('/initializers/')
-end
-
-if relevant_caller_line
-  Rails.logger.info("#{ relevant_caller_line.sub("#{ Rails.root }/", '') }")
-end     
